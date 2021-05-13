1523d25ef948cff76ac42c95e4067873ff7d254d
Ricardo Montenegro <ramn21@gmail.com>
2021-05-13 18:06:55 +0200

    Create model  dog, create migration and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_12_193409) do
+ActiveRecord::Schema.define(version: 2021_05_12_225617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "dogs", force: :cascade do |t|
+    t.string "name"
+    t.string "breed"
+    t.string "owner"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
