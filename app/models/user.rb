299f8f6402c55b10a4c4ece731e022095d9205c0
Ricardo Montenegro <ramn21@gmail.com>
2021-05-14 00:51:45 +0200

    Add has many expenses

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :dogs, foreign_key: :owner
+  has_many :expenses
   validates_presence_of :name, :lastname, :email, :password_digest
 end
