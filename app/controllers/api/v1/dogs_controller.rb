a01a6e95812b6256c5255517fe82035256c0ed9c
Ricardo Montenegro <ramn21@gmail.com>
2021-05-13 18:08:57 +0200

    Add dog controller method create

---
+++ app/controllers/api/v1/dogs_controller.rb
@@ -0,0 +1,14 @@
+class Api::V1::DogsController < ApplicationController
+  before_action :authenticate_and_set_user
+
+  def create
+    @dog = @current_user.dogs.create!(dog_params)
+    render json: @dog.as_json
+  end
+  
+  private
+
+  def dog_params
+    params.require(:dog).permit(:name, :breed)
+  end
+end
