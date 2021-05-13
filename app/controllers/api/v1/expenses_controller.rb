3de9418e654bf4c30fd67d936e94831b0ac7f087
Ricardo Montenegro <ramn21@gmail.com>
2021-05-14 00:51:16 +0200

    Add expense model controller

---
+++ app/controllers/api/v1/expenses_controller.rb
@@ -0,0 +1,24 @@
+class Api::V1::ExpensesController < ApplicationController
+  before_action :authenticate_and_set_user
+
+  def index
+    expenses = Expense.where(user_id: current_user.id).group_by{|u| (u.created_at).strftime('%A-%d-%B')}
+    render json: expenses.as_json
+  end
+
+  def create
+    expense = current_user.expenses.create!(expense_params)
+    render json: expense.as_json, status: :created
+  end
+
+  def destroy
+    expense = Expense.find(params[:id])
+    expense.destroy
+  end
+  
+  private
+
+  def expense_params
+    params.permit(:category, :detail, :amount)
+  end
+end
