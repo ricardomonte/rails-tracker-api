c543aa42dba21e479dd585564cc507b59f0631a4
Ricardo Montenegro <ramn21@gmail.com>
2021-05-15 00:41:11 +0200

    Add serializer in user model,include expenses

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   has_many :dogs, foreign_key: :owner
   has_many :expenses
   validates_presence_of :name, :lastname, :email, :password_digest
+
+  def as_json(options = nil)
+    hash = super(:include => :expenses, :except => [:password_digest, :updated_at, :created_at, :email])
+    return hash
+  end
 end
