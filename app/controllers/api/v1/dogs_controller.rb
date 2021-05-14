4110713a89735bed0448b35198107cfd62a36c21
Ricardo Montenegro <ramn21@gmail.com>
2021-05-14 22:54:59 +0200

    Add index action in dog controller

--- app/controllers/api/v1/dogs_controller.rb
+++ app/controllers/api/v1/dogs_controller.rb
@@ -1,6 +1,11 @@
 class Api::V1::DogsController < ApplicationController
   before_action :authenticate_and_set_user
 
+  def index
+    dogs = Dog.where(owner: current_user.id)
+    render json: dogs.as_json
+  end
+
   def create
     @dog = @current_user.dogs.create!(dog_params)
     render json: @dog.as_json
