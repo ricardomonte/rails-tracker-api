8a362370a6773cf076452ca41bcfda836f08b278
Ricardo Montenegro <ramn21@gmail.com>
2021-05-14 22:51:48 +0200

    Add validation for expense and food

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,3 +1,5 @@
 class Expense < ApplicationRecord
   belongs_to :user
+
+  validates_presence_of :category, :detail, :amount, :user_id
 end
--- app/models/food.rb
+++ app/models/food.rb
@@ -2,6 +2,5 @@ class Food < ApplicationRecord
   belongs_to :dog
 
   validates_presence_of  :name, :amount, :times_per_day, :total_kilograms, :dog_id
-  validates :times_per_day, numericality: {greater_than: 0}
-  validates :total_kilograms, numericality: {greater_than: 0}
+  validates_numericality_of :amount, :times_per_day, :total_kilograms, greater_than: 0
 end
