fe84c3b3417e032fade7d531959b299c1535d2d4
Ricardo Montenegro <ramn21@gmail.com>
2021-05-16 13:38:27 +0200

    Add serializer to model expense

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,4 +2,28 @@ class Expense < ApplicationRecord
   belongs_to :user
 
   validates_presence_of :category, :detail, :amount, :user_id
+
+  def as_json(options = nil)
+    hash = super()
+    hash[:day_of_expense] = expense_day
+    hash[:month_of_expense] = expense_month
+    hash[:year_of_expense] = expense_year
+    return hash
+  end
+
+  private
+
+  def expense_day
+    day = self.created_at.strftime('%-d')
+    return day
+  end
+  def expense_month
+    day = self.created_at.strftime('%-m')
+    return day
+  end
+  def expense_year
+    day = self.created_at.strftime('%Y')
+    return day
+  end
+
 end
