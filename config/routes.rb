5eca2c49f432c3e10f0d6f965a3773d563b97b33
Ricardo Montenegro <ramn21@gmail.com>
2021-05-20 02:57:07 +0200

    Add profile in routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   api_guard_routes for: 'users', controller: {
     registration: 'users/registration',
+    authentication: 'users/authentication'
   }
 
   namespace :api do
@@ -9,8 +10,9 @@ Rails.application.routes.draw do
       resources :dogs, only: [:index, :create] do
         resources :foods, only: [:index, :create, :show]
       end
-      resources :users, only: :show
+      resources :users
       resources :expenses, only: [:index, :create, :show, :destroy]
+      get '/profile', to: "users#profile"
     end
   end
 end
