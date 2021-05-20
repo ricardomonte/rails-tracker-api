3cc2b26fb18852f36dfd234dfc655643f6ad3c96
Ricardo Montenegro <ramn21@gmail.com>
2021-05-20 02:53:34 +0200

    Add profile function

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,4 +5,17 @@ class Api::V1::UsersController < ApplicationController
     user = User.find(params[:id])
     render json: user
   end
+
+  def profile
+    if current_user
+      data_user = {
+        user: current_user,
+        message: "token valid for user #{current_user.id}"
+      }
+      render json: data_user.as_json
+    else
+      payload = { error: 'No token provide' }
+      render json: :payload, status: :unprocessable_entity
+    end
+  end
 end
