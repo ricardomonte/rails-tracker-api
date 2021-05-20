6aa7f151136d2582eddeeb0b9198266cff2cc41e
Ricardo Montenegro <ramn21@gmail.com>
2021-05-20 02:56:36 +0200

    Edit json response in model User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
 
-  has_many :dogs, foreign_key: :owner
-  has_many :expenses
+  has_many :dogs, foreign_key: :owner, dependent: :destroy
+  has_many :expenses,  dependent: :destroy
   validates_presence_of :name, :lastname, :email, :password_digest
 
   def as_json(options = nil)
-    hash = super(:include => :expenses, :except => [:password_digest, :updated_at, :created_at, :email])
+    hash = super(:except => [:password_digest, :updated_at, :created_at, :email])
     return hash
   end
 end
