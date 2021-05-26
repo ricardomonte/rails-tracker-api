626c1586651df724b8c4827b28fbf77bbbdd7637
Ricardo Montenegro <ramn21@gmail.com>
2021-05-26 21:37:57 +0200

    Add format validation for email in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,11 @@ class User < ApplicationRecord
 
   has_many :dogs, foreign_key: :owner, dependent: :destroy
   has_many :expenses, dependent: :destroy
+
   validates_presence_of :name, :lastname, :email, :password_digest
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates_format_of :email, :with => VALID_EMAIL_REGEX
 
   def as_json(_options = nil)
     super(except: %i[password_digest updated_at created_at email])
