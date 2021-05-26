86e75acf130a827cbf201990493b67b44bb8878b
Ricardo Montenegro <ramn21@gmail.com>
2021-05-26 21:18:40 +0200

    Fix problem with databases

--- db/migrate/20210513193733_remove_price_from_food.rb
+++
@@ -1,6 +0,0 @@
-class RemovePriceFromFood < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :foods, :price, :integer
-
-  end
-end
--- db/migrate/20210520105001_drop_table_foods.rb
+++
@@ -1,5 +0,0 @@
-class DropTableFoods < ActiveRecord::Migration[6.1]
-  def change
-    drop_table :foods
-  end
-end
